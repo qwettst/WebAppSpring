6e957569c605efbc6ba8b691bde60224912edfc4
qwettst <qwettst@gmail.com>
2021-02-09 12:54:41 +0700

    0.5.7 minor fix

--- src/test/java/dev/qwett/webappspring/service/impl/ConsumerServiceImplTest.java
+++ src/test/java/dev/qwett/webappspring/service/impl/ConsumerServiceImplTest.java
@@ -1,26 +1,27 @@
 package dev.qwett.webappspring.service.impl;
 
 import dev.qwett.webappspring.entities.Consumer;
-import dev.qwett.webappspring.service.ConsumerService;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class ConsumerServiceImplTest {
 
-    private ConsumerService consumerService;
+    private ConsumerServiceImpl consumerService;
     private ConsumerRepository consumerRepository;
     private Consumer consumer;
 
     @BeforeEach
     void init() {
-        consumerRepository = Mockito.mock(ConsumerRepository.class);
+        consumerRepository = mock(ConsumerRepository.class);
         consumerService = new ConsumerServiceImpl(consumerRepository);
         consumer = new Consumer();
     }
@@ -29,57 +30,70 @@ class ConsumerServiceImplTest {
     @Test
     void findAll() {
         List<Consumer> consumerList = new ArrayList<>();
-        Mockito.when(consumerRepository.findAll()).thenReturn(consumerList);
-        Assert.assertNotNull(consumerRepository.findAll());
-        Mockito.verify(consumerRepository, Mockito.times(1)).findAll();
+        when(consumerRepository.findAll()).thenReturn(consumerList);
+
+        assertNotNull(consumerRepository.findAll());
+
+        verify(consumerRepository, times(1)).findAll();
     }
 
     @Test
     void findById() {
         int id = any(Integer.class);
-        Mockito.when(consumerRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(consumer));
-        Assert.assertNotNull(consumerService.findById(id));
-        Mockito.verify(consumerRepository, Mockito.times(1)).findById(id);
+        when(consumerRepository.findById(id)).thenReturn(Optional.ofNullable(consumer));
+
+        assertNotNull(consumerService.findById(id));
+
+        verify(consumerRepository, times(1)).findById(id);
     }
 
     @Test
     void addConsumer() {
-        Mockito.when(consumerRepository.save(consumer)).thenReturn(consumer);
-        Assert.assertNotNull(consumerService.addConsumer(consumer));
-        Mockito.verify(consumerRepository, Mockito.times(1)).save(consumer);
+        when(consumerRepository.save(consumer)).thenReturn(consumer);
+
+        assertNotNull(consumerService.addConsumer(consumer));
+
+        verify(consumerRepository, times(1)).save(consumer);
     }
 
     @Test
     void updateConsumer() {
-        Mockito.when(consumerRepository.findById(consumer.getIdConsumer())).thenReturn(java.util.Optional.ofNullable(consumer));
-        Mockito.when(consumerRepository.save(consumer)).thenReturn(consumer);
-        Assert.assertNotNull(consumerService.updateConsumer(consumer.getIdConsumer(), consumer));
-        Mockito.verify(consumerRepository, Mockito.times(1)).findById(consumer.getIdConsumer());
-        Mockito.verify(consumerRepository, Mockito.times(1)).save(consumer);
+        when(consumerRepository.findById(consumer.getIdConsumer())).thenReturn(Optional.ofNullable(consumer));
+        when(consumerRepository.save(consumer)).thenReturn(consumer);
+
+        assertNotNull(consumerService.updateConsumer(consumer.getIdConsumer(), consumer));
+
+        verify(consumerRepository, times(1)).findById(consumer.getIdConsumer());
+        verify(consumerRepository, times(1)).save(consumer);
     }
 
     @Test
     void updateConsumerNull() {
-        Mockito.when(consumerRepository.findById(consumer.getIdConsumer())).thenReturn(null);
-        Assert.assertNull(consumerService.updateConsumer(consumer.getIdConsumer(), consumer));
-        Mockito.verify(consumerRepository, Mockito.times(1)).findById(consumer.getIdConsumer());
-        Mockito.verify(consumerRepository, Mockito.times(0)).save(consumer);
+        when(consumerRepository.findById(consumer.getIdConsumer())).thenReturn(null);
+
+        assertNull(consumerService.updateConsumer(consumer.getIdConsumer(), consumer));
+
+        verify(consumerRepository, times(1)).findById(consumer.getIdConsumer());
+        verify(consumerRepository, times(0)).save(consumer);
     }
 
     @Test
     void delete() {
         int id = any(Integer.class);
         consumerService.delete(id);
-        Mockito.verify(consumerRepository, Mockito.times(1)).deleteById(id);
+
+        verify(consumerRepository, times(1)).deleteById(id);
     }
 
     @Test
     void findByName() {
         String str = any(String.class);
         List<Consumer> consumerList = new ArrayList<>();
-        Mockito.when(consumerRepository.findByName(str)).thenReturn(consumerList);
-        Assert.assertNotNull(consumerService.findByName(any(String.class)));
-        Mockito.verify(consumerRepository, Mockito.times(1)).findByName(str);
+        when(consumerRepository.findByName(str)).thenReturn(consumerList);
+
+        assertNotNull(consumerService.findByName(any(String.class)));
+
+        verify(consumerRepository, times(1)).findByName(str);
 
     }
 }
--- src/test/java/dev/qwett/webappspring/service/impl/StoreServiceImplTest.java
+++ src/test/java/dev/qwett/webappspring/service/impl/StoreServiceImplTest.java
@@ -1,18 +1,18 @@
 package dev.qwett.webappspring.service.impl;
 
 import dev.qwett.webappspring.entities.Store;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class StoreServiceImplTest {
-
 
     private StoreServiceImpl storeService;
     private StoreRepository storeRepository;
@@ -20,7 +20,7 @@ class StoreServiceImplTest {
 
     @BeforeEach
     void init() {
-        storeRepository = Mockito.mock(StoreRepository.class);
+        storeRepository = mock(StoreRepository.class);
         storeService = new StoreServiceImpl(storeRepository);
         store = new Store();
     }
@@ -28,56 +28,68 @@ class StoreServiceImplTest {
     @Test
     void findAll() {
         List<Store> storeList = new ArrayList<>();
-        Mockito.when(storeRepository.findAll()).thenReturn(storeList);
-        Assert.assertNotNull(storeService.findAll());
-        Mockito.verify(storeRepository, Mockito.times(1)).findAll();
+        when(storeRepository.findAll()).thenReturn(storeList);
+
+        assertNotNull(storeService.findAll());
+
+        verify(storeRepository, times(1)).findAll();
     }
 
     @Test
     void findById() {
         int id = any(Integer.class);
-        Mockito.when(storeRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(store));
-        Assert.assertNotNull(storeService.findById(id));
-        Mockito.verify(storeRepository, Mockito.times(1)).findById(id);
+        when(storeRepository.findById(id)).thenReturn(java.util.Optional.ofNullable(store));
+
+        assertNotNull(storeService.findById(id));
+
+        verify(storeRepository, times(1)).findById(id);
     }
 
     @Test
     void addStore() {
-        Mockito.when(storeRepository.save(any(Store.class))).thenReturn(store);
-        Assert.assertNotNull(storeService.addStore(store));
-        Mockito.verify(storeRepository, Mockito.times(1)).save(store);
+        when(storeRepository.save(any(Store.class))).thenReturn(store);
+
+        assertNotNull(storeService.addStore(store));
+
+        verify(storeRepository, times(1)).save(store);
     }
 
     @Test
     void updateStoreNull() {
-        Mockito.when(storeRepository.findById(store.getIdStore())).thenReturn(null);
-        Assert.assertNull(storeService.updateStore(store.getIdStore(), store));
-        Mockito.verify(storeRepository, Mockito.times(1)).findById(store.getIdStore());
-        Mockito.verify(storeRepository, Mockito.times(0)).save(store);
+        when(storeRepository.findById(store.getIdStore())).thenReturn(null);
+
+        assertNull(storeService.updateStore(store.getIdStore(), store));
+
+        verify(storeRepository, times(1)).findById(store.getIdStore());
+        verify(storeRepository, times(0)).save(store);
     }
 
     @Test
     void updateStore() {
-        Mockito.when(storeRepository.findById(store.getIdStore())).thenReturn(java.util.Optional.ofNullable(store));
-        Mockito.when(storeRepository.save(any(Store.class))).thenReturn(store);
-        Assert.assertNotNull(storeService.updateStore(store.getIdStore(), store));
-        Mockito.verify(storeRepository, Mockito.times(1)).findById(store.getIdStore());
-        Mockito.verify(storeRepository, Mockito.times(1)).save(store);
+        when(storeRepository.findById(store.getIdStore())).thenReturn(java.util.Optional.ofNullable(store));
+        when(storeRepository.save(any(Store.class))).thenReturn(store);
+
+        assertNotNull(storeService.updateStore(store.getIdStore(), store));
+
+        verify(storeRepository, times(1)).findById(store.getIdStore());
+        verify(storeRepository, times(1)).save(store);
     }
 
     @Test
     void findByAddress() {
         String str = any(String.class);
         List<Store> storeList = new ArrayList<>();
-        Mockito.when(storeRepository.findByAddress(str)).thenReturn(storeList);
-        Assert.assertNotNull(storeService.findByAddress(any(String.class)));
-        Mockito.verify(storeRepository, Mockito.times(1)).findByAddress(str);
+        when(storeRepository.findByAddress(str)).thenReturn(storeList);
+
+        assertNotNull(storeService.findByAddress(any(String.class)));
+
+        verify(storeRepository, times(1)).findByAddress(str);
     }
 
     @Test
     void delete() {
         int id = any(Integer.class);
         storeService.delete(id);
-        Mockito.verify(storeRepository, Mockito.times(1)).deleteById(id);
+        verify(storeRepository, times(1)).deleteById(id);
     }
 }
